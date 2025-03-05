f719860c6e776af01afebd87455882ad26f21519
ncrqnt <44952997+ncrqnt@users.noreply.github.com>
2025-03-05 13:03:39 +0100

    Update landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,44 @@
-import Hero from "@/components/hero";
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+import { ThemeSwitcher } from "@/components/theme-switcher";
+import AuthButton from "@/components/header-auth";
 
 export default async function Home() {
-  return (
-    <>
-      <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-      </main>
-    </>
-  );
+    return (
+        <>
+            <main className="flex min-h-screen w-full flex-col items-center">
+                <div className="flex-1 w-full flex flex-col items-center bg-background">
+                    <header
+                        className="sticky top-0 w-full flex justify-center border-b border-b-foreground/10 h-16 bg-background z-50">
+                        <div className="w-full flex justify-between items-center p-3 px-5 text-sm">
+
+                            <div className="flex gap-5 items-center font-semibold">
+                                <span>Songlinks</span>
+                            </div>
+                            <div className="flex gap-5 items-center">
+                                <AuthButton />
+                                <ThemeSwitcher/>
+                            </div>
+                        </div>
+                    </header>
+                    <div className="flex flex-1 flex-col max-w-full p-5 w-full items-center justify-center">
+                        <h1 className="text-4xl font-bold mb-4">Songlinks</h1>
+                        <h2 className="text-2xl font-light italic mb-8">Fancy landing page</h2>
+                        <p>Login to get started</p>
+                    </div>
+                </div>
+                <footer
+                    className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 h-16">
+                    <p>
+                        <a
+                            href="https://github.com/ncrqnt/weblab-projekt"
+                            target="_blank"
+                            className="font-bold hover:underline"
+                            rel="noreferrer"
+                        >
+                            GitHub
+                        </a>
+                    </p>
+                </footer>
+            </main>
+        </>
+    );
 }
